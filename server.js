cdab4feb3777ab01f9bd07a5dbed7b38ccd46d1c
samson wong <steelerwong2005@hotmail.com>
2020-10-05 21:08:14 -0400

    updated server to add app.get

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(routes)
 //Connet to mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
